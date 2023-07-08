e5947ffbd93bd249efbd89754a29dd03fcbeb1b9
Vivek-Prajapatii <prajapativivek415@gmail.com>
2023-07-08 19:06:46 +0530

    navigation list added

--- src/components/Header.js
+++ src/components/Header.js
@@ -18,21 +18,61 @@ const Header = () => {
           </SearchIcon>
         </Search>
         <Nav>
-          <NavHome>
-            <img src="/images/nav-home.svg" alt="home" />
-          </NavHome>
-          <NavNetwork>
-            <img src="/images/nav-network.svg" alt="network" />
-          </NavNetwork>
-          <NavJobs>
-            <img src="/images/nav-jobs.svg" alt="jobs" />
-          </NavJobs>
-          <NavMessaging>
-            <img src="/images/nav-messaging.svg" alt="messaging" />
-          </NavMessaging>
-          <NavNotifications>
-            <img src="/images/nav-notifications.svg" alt="notifications" />
-          </NavNotifications>
+          <NavListWrap>
+            <NavList className="active ">
+              <a>
+                <img src="/images/nav-home.svg" />
+                <span>Home</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a>
+                <img src="/images/nav-network.svg" />
+                <span>My Network</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a>
+                <img src="/images/nav-jobs.svg" />
+                <span>Jobs</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a>
+                <img src="/images/nav-messaging.svg" />
+                <span>Messaging</span>
+              </a>
+            </NavList>
+            <NavList>
+              <a>
+                <img src="/images/nav-notifications.svg" />
+                <span>Notifications</span>
+              </a>
+            </NavList>
+
+            <User>
+              <a>
+                <img src="/images/user.svg" />
+                <span>
+                  Me
+                  <img src="/images/down-icon.svg" />
+                </span>
+              </a>
+
+              <SignOut>
+                <a>Sign Out</a>
+              </SignOut>
+            </User>
+            <Work>
+              <a>
+                <img src="/images/nav-work.svg" />
+                <span>
+                  Work
+                  <img src="/images/down-icon.svg" />
+                </span>
+              </a>
+            </Work>
+          </NavListWrap>
         </Nav>
       </Content>
     </Container>
@@ -45,7 +85,6 @@ const Container = styled.div`
   left: 0;
   padding: 0 24px;
   position: fixed;
-  height: 45px;
   width: 100vw;
   top: 0;
   z-index: 100;
@@ -104,39 +143,119 @@ const SearchIcon = styled.div`
 `;
 
 const Nav = styled.div`
-  width: fit-content;
-  margin-left: 80px;
+  margin-left: auto;
+  display: block;
+
+  @media (max-width: 768px) {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    background: white;
+    width: 100%;
+  }
+`;
+
+const NavListWrap = styled.ul`
   display: flex;
+  flex-wrap: wrap;
+  list-style-type: none;
+
+  .active {
+    span:after {
+      content: "";
+      transform: scale(1);
+      border-bottom: 2px solid var(--white, #fff);
+      bottom: 0;
+      left: 0;
+      position: absolute;
+      transition: transfprm 0.2s ease-in-out;
+      width: 100%;
+      border-color: rgba(0, 0, 0, 0.9);
+    }
+  }
 `;
 
-const NavHome = styled.div`
-  text-align: left;
-  /* width: fit-content; */
-  margin-right: 15px;
+const NavList = styled.li`
+  display: flex;
+  align-items: center;
+  a {
+    align-items: center;
+    background: transparent;
+    display: flex;
+    flex-direction: column;
+    font-size: 12px;
+    font-weight: 400;
+    justify-content: center;
+    line-height: 1.5;
+    min-height: 42px;
+    min-width: 80px;
+    position: relative;
+    text-decoration: none;
+
+    span {
+      color: rgba(0, 0, 0, 0.6);
+      display: flex;
+      align-items: center;
+    }
+
+    @media (max-width: 768px) {
+      min-width: 70px;
+    }
+  }
+
+  &:hover,
+  &:active {
+    a {
+      span {
+        color: rgba(0, 0, 0, 0.9);
+      }
+    }
+  }
 `;
 
-const NavNetwork = styled.div`
-  text-align: left;
-  /* width: fit-content; */
-  margin-right: 15px;
+const SignOut = styled.div` 
+  position: absolute;
+  top: 45px;
+  background: white;
+  border-radius: 0 0 5px 5px;
+  width: 100px;
+  height: 40px;
+  font-size: 16px;
+  transition-duration: 167ms;
+  text-align: center;
+  display: none;
 `;
 
-const NavJobs = styled.div`
-  text-align: left;
-  /* width: fit-content; */
-  margin-right: 15px;
+const User = styled(NavList)`
+  a > svg {
+    width: 24px;
+    border-radius: 50%;
+  }
+
+  a > img {
+    width: 24px;
+    height: 24px;
+    border-radius: 50%;
+  }
+
+  span {
+    display: flex;
+    align-items: center;
+  }
+
+  &:hover{
+    ${SignOut} {
+      align-items: center;
+      display: flex;
+      justify-content: center;
+    }
+  }
+
 `;
 
-const NavMessaging = styled.div`
-  text-align: left;
-  /* width: fit-content; */
-  margin-right: 15px;
+const Work = styled(User)`
+  border-left: 1px solid rgba(0, 0, 0, 0.08);
 `;
 
-const NavNotifications = styled.div`
-  text-align: left;
-  /* width: fit-content; */
-  margin-right: 15px;
-`;
 
 export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,35 @@ import { styled } from "styled-components";
 
 const Home = () => {
     return (
-        <div>Home</div>
+        <Container>
+            <Layout>
+                <div>leftside</div>
+                <div>middle</div>
+                <div>right side</div>
+            </Layout>
+        </Container>
     );
 }
+
+const Container = styled.div`
+    padding-top: 52px;
+    max-width: 100%;
+`;
+
+const Layout = styled.div`
+    display: grid;
+    grid-template-areas: "leftside main rightside";
+    grid-template-columns: minmax(0, 5fr) minmax(0, 12fr) minmax(300px, 7fr);
+    column-gap: 25px;
+    row-gap: 25px;
+    grid-template-rows: auto;
+    margin: 25px 0;
+
+    @media (max-width: 768px) {
+        display: flex;
+        flex-direction: column;
+        padding: 0 5px;
+    }
+`;
 
 export default Home;
