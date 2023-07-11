d032e978dc79a726889d0c1616c6f9330830a1ec
Vivek-Prajapatii <prajapativivek415@gmail.com>
2023-07-12 00:25:34 +0530

    .

--- src/components/LinkedinNews.js
+++ src/components/LinkedinNews.js
@@ -6,7 +6,28 @@ const LinkedinNews = () => {
       <FollowCard>
         <Title>
           <h2>Add to your feed</h2>
+          <img src="/images/feed-icon.svg" />
         </Title>
+        <FeedList>
+          <li>
+            <a>
+              <Avatar />
+            </a>
+            <div>
+              <span>#Linkedin</span>
+              <button>Follow</button>
+            </div>
+          </li>
+          <li>
+            <a>
+              <Avatar />
+            </a>
+            <div>
+              <span>#Video</span>
+              <button>Follow</button>
+            </div>
+          </li>
+        </FeedList>
       </FollowCard>
     </Container>
   );
@@ -26,6 +47,52 @@ const FollowCard = styled.div`
   border: none;
   box-shadow: 0 0 0 1px rgb(0 0 0 /15%), 0 0 0 rgb(0 0 0 / 20%);
   padding: 12px;
+`;
+
+const Title = styled.div`
+  display: inline-flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 16px;
+  width: 100%;
+  color: rgba(0, 0, 0, 0.6);
+`;
+
+const FeedList = styled.ul`
+  margin-top: 16px;
+  li {
+    display: flex;
+    align-items: center;
+    margin: 12px 0;
+    position: relative;
+    font-size: 14px;
+
+    & > div {
+      display: flex;
+      flex-direction: column;
+    }
+
+    bottom {
+      background-color: transparent;
+      color: rgba(0,0,0,0.6);
+      box-shadow: inset 0 0 0 1px rgba(0,0,0,0.6);
+      padding: 16px;
+      align-items: center;
+      border-radius: 15px;
+      box-sizing: border-box;
+      font-weight: 600;
+      display: inline-flex;
+      justify-content: center;
+      max-height: 32px;
+      max-width: 480px;
+      text-align: center;
+      outline: none;
+    }
+  }
+`;
+
+const Avatar = styled.div`
+  background-image: url(" ");
 `;
 
 export default LinkedinNews;
